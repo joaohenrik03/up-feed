fae48ec78349f5c38b4ae87501866cc0ac61fff9
João Henrik <jh.signori@gmail.com>
2022-08-17 18:41:44 -0300

    Avatar component created and styled.

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,16 @@
+import styles from './Avatar.module.css';
+
+type AvatarProps = {
+    src: string;
+    hasBorder?: boolean;
+};
+
+export function Avatar({ src, hasBorder = true }: AvatarProps) {
+    return (
+        <img 
+            className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+            src={src}
+            alt="Foto do usuário" 
+        />
+    )
+}
